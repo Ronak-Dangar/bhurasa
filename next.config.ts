0bdbdf538722361316ab802d097666f9bd283ba5
RonakDangar <ronakdangar04@gmail.com>
2026-05-24 15:20:41 +0530

    Add redirect from / to bhurasa.reneva.in

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: require("path").join(__dirname, "./"),
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://bhurasa.reneva.in',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
